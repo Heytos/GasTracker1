883ec7613357bc9bac4a20383e21bbd0f43b2b9c
Heytos <hayweaky@gmail.com>
2023-08-08 12:51:37 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,5 +3,5 @@ url = 'https://api.etherscan.io/api'
 params = {
     "module": "gastracker",
     "action": "gasoracle",
-    "apikey": "FYNDGBGSEY14A1Q2P1APZQ6MMI6V5DCQD6"
+    "apikey": "YOUR_API_KEYS"
 }
