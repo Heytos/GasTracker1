102e99d750bed81f503f6592437f85e0899c837d
Heytos <hayweaky@gmail.com>
2023-08-08 19:50:10 +0300

    Правки

--- GasTracker.py
+++ GasTracker.py
@@ -3,18 +3,19 @@ import json
 import configs
 from requests.exceptions import JSONDecodeError
 
+def test():
+    print('Ethgdsf')
+
 def gas_tracker():
     response = requests.get(configs.url, params=configs.params)
+
+    if response.status_code != 200:
+        return 'Возникла ошибка, попробуйте позднее'
 
     try:
         data = response.json()
     except JSONDecodeError:
         return "Ошибка при декодировании ответа в формате JSON."
-
-
-    slow_gas_price = data['result']['SafeGasPrice']
-    mid_gas_price = data['result']['ProposeGasPrice']
-    fast_gas_price = data['result']['FastGasPrice']
 
     if 'result' not in data or not isinstance(data['result'], dict):
         return 'Возникла ошибка, переменные не найдены'
@@ -24,9 +25,11 @@ def gas_tracker():
         if key not in data['result']:
             return f'Возникла ошибка {key} не найден'
 
-    if response.status_code != 200:
-        return 'Возникла ошибка, попробуйте позднее'
-    else:
-        return f"Low: {slow_gas_price} gwei\nAvg: {mid_gas_price} gwei\nHigh: {fast_gas_price} gwei"
+    slow_gas_price = data['result']['SafeGasPrice']
+    mid_gas_price = data['result']['ProposeGasPrice']
+    fast_gas_price = data['result']['FastGasPrice']
+
+
+    return f"Low: {slow_gas_price} gwei\nAvg: {mid_gas_price} gwei\nHigh: {fast_gas_price} gwei"
 
 print(gas_tracker())
