4e893932ce37498fe1e1ec26b4ab31f2668ab399
Heytos <hayweaky@gmail.com>
2023-08-11 13:19:24 +0300

    надеюсь финальная версия

--- PriceTracker.py
+++ PriceTracker.py
@@ -1,6 +1,5 @@
 import requests
-
-
+from requests.exceptions import HTTPError
 def price_check(symbol):
     url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
     parameters = {
@@ -9,13 +8,26 @@ def price_check(symbol):
     }
     headers = {
         'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': 'YOUR_API_KEY',
+        'X-CMC_PRO_API_KEY': 'df269fa7-d67c-463d-88fc-b3c4e2c617bc',
     }
 
     response = requests.get(url, params=parameters, headers=headers)
-    response.raise_for_status()
 
     data = response.json()
+
+    try:
+        response.raise_for_status()
+        price = data['data'][symbol]["quote"]['USD']['price']
+        percent_change_24h = data['data'][symbol]['quote']['USD']['percent_change_24h']
+    except HTTPError:
+        print ('Ошибка в получении данных API')
+        return None, None
+    except ValueError:
+        print ('Произошла ошибка, попробуйте позднее')
+        return None, None
+    except KeyError:
+        print ('Произошла ошибка, попробуйте позднее')
+        return None, None
 
     price = data['data'][symbol]["quote"]['USD']['price']
     percent_change_24h = data['data'][symbol]['quote']['USD']['percent_change_24h']
@@ -26,8 +38,12 @@ if __name__ == "__main__":
     token_symbol = input(
         "Введите криптовалюту: ").upper()
     price, percent_change_24h = price_check(token_symbol)
-    print(f'Цена {token_symbol} {price:.2f} $')
-    if percent_change_24h > 0:
-        print(f'+{percent_change_24h:.2f}%')
+
+    if price is None or percent_change_24h is None:
+        print("Не удалось получить данные для", token_symbol)
     else:
-        print(f' {percent_change_24h:.2f}%')
+        print(f'Цена {token_symbol} {price:.2f} $')
+        if percent_change_24h > 0:
+            print(f'+{percent_change_24h:.2f}%')
+        else:
+            print(f' {percent_change_24h:.2f}%')
